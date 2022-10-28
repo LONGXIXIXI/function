57a5b5fbe5b5d7a8367d61c0492bb0b23329cd98
LONGXIXIXI <115448031+LONGXIXIXI@users.noreply.github.com>
2022-10-28 15:53:59 +0800

    Add files via upload

---
+++ Fibonacci sequence 2.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>               //斐波那契数列
+int fib(int x)
+{
+	int a=1;
+	int b=1;
+	int c=1;
+	while(x>=3)
+	{
+		c=a+b;
+		a=b;
+		b=c;
+		x--;
+	}
+	return b;
+}
+int main()
+{
+	int n=0;
+	int ret=0;
+	scanf("%d",&n);
+	ret=fib(n);
+	printf("第%d个斐波那契数为%d\n",n,ret);
+	return 0;
+}
